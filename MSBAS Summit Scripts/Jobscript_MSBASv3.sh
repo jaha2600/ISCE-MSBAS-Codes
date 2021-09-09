7931f560e33ea0255e7dfb36bf9d5f452a574841
jaha2600 <jasmine.hansen@colorado.edu>
2021-09-09 11:49:02 -0600

    bash script for msbas on summit

---
+++ MSBAS Summit Scripts/Jobscript_MSBASv3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Written by: jaha2600@colorado.edu
+# Date: 20210909
+# Purpose: This script submits a job on Summit. Has been edited to accept a 5m local DEM instead of downloading one. 
+# It is possible that the python script still downloads one, but doesn't use it.  
+#SBATCH -A ucb-summit-mjw     # Summit allocation
+#SBATCH --partition=shas     # Summit partition
+#SBATCH --qos=normal                 # Summit qos
+#SBATCH --time=024:00:00           # Max wall time
+#SBATCH --nodes=1            # Number of Nodes
+#SBATCH --ntasks=2           # Number of tasks per job
+
+#SBATCH --job-name=aptinsar        # Job submission name
+#SBATCH --output=aptinsar.%A_%a.out   # Output file name with Job ID
+
+#SBATCH --mail-type=END            # Email user when job finishes
+#SBATCH --mail-user=jaha2600@colorado.edu # Email address of user
+
+
+#purge all existing modules
+module purge
+# load modules needed to run program 
+module load singularity/3.6.4
+
+#make path to header file the first commandline argument 
+HEADER_FILE=$1
+
+#run msbas program with header file as argument 
+singularity exec /projects/jaha2600/containers/msbasv3.sif msbasv3 $HEADER_FILE
+
