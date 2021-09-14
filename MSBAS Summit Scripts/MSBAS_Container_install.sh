3d62630958bb3b073edfd01c514f760be2258598
jaha2600 <jasmine.hansen@colorado.edu>
2021-09-14 12:41:07 -0600

    changes

--- MSBAS Summit Scripts/MSBAS_Container_install.sh
+++ MSBAS Summit Scripts/MSBAS_Container_install.sh
@@ -9,5 +9,10 @@ module load singularity
 export SINGULARITY_TMPDIR=/scratch/summit/$USER
 export SINGULARITY_CACHEDIR=/scratch/summit/$USER
 
-singularity pull --name msbasv3.sif singularity pull library://monaghaa/default/msbasv3:latest
+singularity pull msbasv3.sif library://monaghaa/default/msbasv3:latest
+
+#singularity pull --name msbasv3.sif
+
+#singularity pull library://monaghaa/default/msbasv3:latest
+
 echo 'Done creating msbas container in /projects/$USER/containers'
